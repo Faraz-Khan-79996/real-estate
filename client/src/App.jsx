19d5dadd7338410c6101aad7f85dab29bcf9f2c6
Faraz-Khan-79996 <farazgod1234@gmail.com>
2024-07-05 15:21:30 +0530

    router and routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1 className='text-7xl font-bold'>Hello Faraz the great</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sign-in" element={<Signin/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/profile" element={<Profile/>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
