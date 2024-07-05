6c236a64f48e688e7e8c7cfa88073ccd667f4947
Faraz-Khan-79996 <farazgod1234@gmail.com>
2024-07-05 15:10:13 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-7xl font-bold'>Hello Faraz the great</h1>
+    </>
+  )
+}
+
+export default App
