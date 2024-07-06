520ca2de14b65129c01a701fb3eac937b4b5f910
Faraz-Khan-79996 <farazgod1234@gmail.com>
2024-07-07 00:06:10 +0530

    image upload feature added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   
@@ -21,7 +22,11 @@ function App() {
           <Route path="/sign-in" element={<Signin/>} />
           <Route path="/sign-up" element={<SignUp/>} />
           <Route path="/about" element={<About/>} />
-          <Route path="/profile" element={<Profile/>} />
+
+          <Route element={<PrivateRoute/>}>
+            <Route path="/profile" element={<Profile/>} />
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet , Navigate } from 'react-router-dom'
+
+function PrivateRoute() {
+
+    const {currentUser} = useSelector(state => state.user)
+
+    if(currentUser){
+        return <Outlet />
+    }
+    else{
+        return <Navigate to ="/sign-in"/>
+    }
+}
+
+export default PrivateRoute 
