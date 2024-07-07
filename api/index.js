c352ec3ad30b2f40f47b476df0ae0b23376df611
Faraz-Khan-79996 <farazgod1234@gmail.com>
2024-07-07 23:32:10 +0530

    all features completed, Home page completed, about page completed

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import UserRouter from './routes/user.route.js'
 import AuthRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 
 
 main().catch(err => console.log(err));
@@ -16,13 +16,21 @@ async function main() {
     console.log("database connected");
 }
 
+const __dirname = path.resolve();
+
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.use('/api/user' , UserRouter)
 app.use('/api/auth' , AuthRouter)
 app.use('/api/listing' , listingRouter)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err , req , res , next)=>{
     const statusCode = err.statusCode || 500;
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Estate-Vista
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Estate-Vista is your premier destination for buying, selling, and
+        renting properties in the most sought-after locations. Our dedicated
+        team of seasoned agents is committed to delivering exceptional service
+        and ensuring a seamless real estate experience.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission at Estate-Vista is to help you achieve your real estate
+        aspirations by offering expert guidance, personalized service, and an
+        in-depth knowledge of the local market. Whether you're looking to
+        purchase, sell, or rent, we are here to support you through every stage
+        of the process.
+      </p>
+      <p className="mb-4 text-slate-700">
+        With extensive experience and expertise in the real estate industry, our
+        agents are devoted to providing the highest level of service. At
+        Estate-Vista, we believe that the journey of buying or selling a
+        property should be enjoyable and fulfilling, and we strive to make that
+        a reality for each of our clients.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
