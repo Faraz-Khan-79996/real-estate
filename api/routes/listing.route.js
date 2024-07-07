a14b47cee088c308ff0e8eaba190720943f702fc
Faraz-Khan-79996 <farazgod1234@gmail.com>
2024-07-07 18:02:28 +0530

    udpate listing page created on the frontend. getListing route created to get listing from id

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing , deleteListing , updateListing } from '../controllers/listing.controller.js';
+import { createListing , deleteListing , updateListing , getListing } from '../controllers/listing.controller.js';
 import { isLoggedIn } from '../utils/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create', isLoggedIn, createListing);
 router.delete('/delete/:id', isLoggedIn, deleteListing);
 router.post('/update/:id', isLoggedIn, updateListing);
+router.get('/get/:id', getListing);
 
 export default router;
