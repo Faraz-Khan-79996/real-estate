87fe647462f44d75a083bdbe09db37efbf336245
Faraz-Khan-79996 <farazgod1234@gmail.com>
2024-07-07 19:58:33 +0530

    search route created for search.

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing , deleteListing , updateListing , getListing } from '../controllers/listing.controller.js';
+import { createListing , deleteListing , updateListing , getListing , getListings } from '../controllers/listing.controller.js';
 import { isLoggedIn } from '../utils/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/create', isLoggedIn, createListing);
 router.delete('/delete/:id', isLoggedIn, deleteListing);
 router.post('/update/:id', isLoggedIn, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 export default router;
